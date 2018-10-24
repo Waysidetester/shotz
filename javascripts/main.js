bfbaa647820d716ef9e273b91409a173c8e3285d
WaysideTester <dylanmurray211@outlook.com>
2018-10-23 21:29:41 -0500

    fixed case sensitivity issue

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,14 @@ import locationComponent from "./components/locationComponent.js";
 import movieComponent from "./components/movieComponent.js";
 import events from "./events.js";
 
+$.expr[':'].contains = function(a, i, m) {
+    return $(a).text().toUpperCase()
+        .indexOf(m[3].toUpperCase()) >= 0;
+  };
+
 const init = () => {
-    movieComponent.movCompExporter();
     locationComponent.locCompExporter();
+    movieComponent.movCompExporter();
     events.eventApplyer();
 }
 
